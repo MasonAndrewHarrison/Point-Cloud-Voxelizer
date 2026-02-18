89480ff457e18631316cf92f754b40ded567161c
MasonAndrewHarrison <masonandrewharrison@gmail.com>
2026-02-18 15:51:59 -0500

    commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ label_dataset = np.load('Label.npy')
 
 for i in range(102):
 
-    model = Dataset.load_data(f"Voxel_Dataset_32p/model{i}.npy")
+    model = Dataset.load_data(f"Voxel_Dataset_50p/model{i}.npy")
     point_cloud = Dataset.convert_to_point_cloud(model)  
 
     print(label_dataset[i])
